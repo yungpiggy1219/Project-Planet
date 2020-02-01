82deacbda7fb738c1083599af9856216bbebabc0
lchiu005 <44789905+lchiu005@users.noreply.github.com>
2020-02-01 04:11:00 -0800

    started on classes

---
+++ CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Vector3 dragOrigin;
+    private float dragSpeed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed, 0, 0);
+
+        transform.Translate(move, Space.World);
+    }
+}
---
+++ CameraShrinkScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShrinkScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // shrink relative to camera position
+        float hyp = Vector3.Distance(gameObject.transform.localPosition, Camera.main.transform.localPosition); // hypo
+        // distance is closer, scale is bigger
+        hyp = 100 / Mathf.Sqrt(hyp);
+        print(hyp);
+        float size_sphere = 8;
+        gameObject.transform.localScale = new Vector3(hyp*size_sphere, hyp*size_sphere, hyp*size_sphere);
+    }
+}
