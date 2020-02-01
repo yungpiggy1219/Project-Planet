65d3548f1e4e578cc628efc6278f6e42edf18161
lchiu005 <44789905+lchiu005@users.noreply.github.com>
2020-02-01 04:38:37 -0800

    almost finished planet implement.

--- Planet.cs
+++ Planet.cs
@@ -2,17 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Planet : MonoBehaviour
+public class Planet:MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Environment env;
+    private Resource[] resources_polluted;
+    private Resource[] resources;
+    private SpriteRenderer sprite;
+
+    public Planet(Environment env, Resource[] rp, Resource[]r)
     {
-        
+        this.env = env;
+        resources_polluted = rp;
+        resources = r;
     }
 
-    // Update is called once per frame
-    void Update()
+    private Resource[] getResources()
     {
-        
+        return resources;
     }
+
+    private Resource[] getResourcesPolluted()
+    {
+        return resources_polluted;
+    }
+
+    private int pollute(int resource_type, int amount) // function that removes resourses at index and adds them to resources_polluted
+    {
+        if (resources[resource_type].getCount() >= amount){ // if there are more resources than deduction amount
+            resources[resource_type].remove(amount);
+            resources_polluted[resource_type].allocate(amount);
+            return 0; // all can be deducted here, return 0
+        }
+        else { // otherwise if there isn't enough to deduct from this planet
+            int dum = resources[resource_type].getCount(); 
+            resources[resource_type].remove(resources[resource_type].getCount());
+            resources_polluted[resource_type].allocate(dum);
+            return amount - dum; // return non-zero integer which represents deductions to be made
+        }
+    }
+
+    private void abate(int resource_type, int amount)
+    {
+        resources_polluted[resource_type].remove(amount);
+        resources[resource_type].allocate(amount);
+    }
+
 }
--- Resource.cs
+++ Resource.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Resource : MonoBehaviour
+public class Resource
 {
     private SpriteRenderer sprite;
-    // Start is called before the first frame update
-    void Start()
+    private string resource_type;
+    private int count;
+    public Resource(string resource_type, int count)
     {
-        
+        this.resource_type = resource_type;
+        this.count = count;
     }
-
-    // Update is called once per frame
-    void Update()
+    public int getCount()
     {
-        
+        return count;
+    }
+    public void allocate(int num)
+    {
+        count += num;
+    }
+    public void remove(int num)
+    {
+        count -= num;
+    }
+    public bool atLeast(int num)
+    {
+        return num <= count;
     }
 }
